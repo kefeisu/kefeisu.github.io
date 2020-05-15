36fae33f5d62df142c71007389db7b3379a470d1
KeFei <kefeisu@gmail.com>
2020-05-15 00:18:40 -0400

    add social media links & icons

--- assets/scipts/toggle.js
+++ assets/scipts/toggle.js
@@ -1,11 +1,26 @@
-function toggleProfilePicture() {
+function toggleTheme() {
     var theme_cb = document.getElementById("toggle-mode-cb").checked;
     var image = document.getElementById("profile-pic");
+
+    var headline_light = document.getElementById("headline--light");
+    var headline_dark = document.getElementById("headline--dark");
+    var media_icons = document.getElementsByClassName("media-icon");
+
     if(theme_cb) {
         //image.src = "assets/img/kefei_light_theme.png"
-        image.src = "assets/img/kefei_light_theme.jpg"
+        image.src = "assets/img/kefei_light_theme.jpg";
+        headline_light.style.display = "inline";
+        headline_dark.style.display = "none";
+        for (var i = 0; i < media_icons.length; i++) {
+            media_icons[i].style.filter = "none";
+        }
     } else {
         //image.src = "assets/img/kefei_dark_theme.png"
-        image.src = "assets/img/kefei.jpg"
+        image.src = "assets/img/kefei.jpg";
+        headline_light.style.display = "none";
+        headline_dark.style.display = "inline";
+        for (var i = 0; i < media_icons.length; i++) {
+            media_icons[i].style.filter = "invert(100%)";
+        }
     }
 };
